9068c9d61048cd8cfd7c9c7ff812e06ef51d99aa
josippozega <96910194+josippozega@users.noreply.github.com>
2022-01-04 11:46:28 +0100

    Update vrijeme.py

--- vrijeme.py
+++ vrijeme.py
@@ -95,4 +95,5 @@ if city_name:
             df['Max temp'] = max_temp_df
             st.table(df)
 
-	st.map(pd.DataFrame({'lat' : [res[5]] , 'lon' : [res[4]]},columns = ['lat','lon']))
+	st.map(pd.DataFrame
+	       ({'lat' : [res[5]] , 'lon' : [res[4]]},columns = ['lat','lon']))
